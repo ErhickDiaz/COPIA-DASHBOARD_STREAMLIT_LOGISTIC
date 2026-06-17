285571153f1a7e49c391b2e5f7cc67f2f87e61c6
ErhickDiaz <erick.diaz@grupobimbo.com>
2026-06-16 20:35:24 -0400

    Update logistica_inversa.py
    up

--- logistica_inversa.py
+++ logistica_inversa.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+import streamlit as st
+
+def main():
+    st.title("♻️ Logística Inversa")
+    st.success("✅ Módulo cargado correctamente")
+
 st.set_page_config(
     page_title="Dashboard Logística Inversa",
     layout="wide"
